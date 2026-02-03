47f8daa4c8a7b5c535db9c1e6250c181cf85e117
CodeByArtem <150847319+CodeByArtem@users.noreply.github.com>
2026-02-03 08:29:45 +0100

    Add JSON storage

--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import * as dayjs from 'dayjs';
+import * as customParseFormat from 'dayjs/plugin/customParseFormat';
 import * as fs from 'fs';
 import * as path from 'path';
 
